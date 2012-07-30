ed90549ce0042af005118792ff11f3487bdd0bca
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-30 02:54:52 -0400

    Updating the example default node configuration in site.pp.

--- example/site.pp
+++ example/site.pp
@@ -9,7 +9,7 @@
  * TO USE:
  *
  * Copy this file to your root manifest directory.  It should be the only node
- * on the system.  In order to change what puppet code is run on a partiular
+ * on the system.  In order to change what puppet code is run on a particular
  * server, the "profiles" property should be used.
  */
 node default {
@@ -18,12 +18,10 @@ node default {
   import "core/*.pp"
   include data::common
 
-  $common_config = $data::common::os_hiera_common_config
-
   debug "Hiera ready: ${::hiera_ready}"
-  debug "Common configuration file: ${common_config}"
+  debug "Common configuration file: ${data::common::os_hiera_common_config}"
 
-  if ! ( $::hiera_ready and exists($common_config) ) {
+  if ! ( $::hiera_ready and exists($data::common::os_hiera_common_config) ) {
     notice "Bootstrapping server"
 
     # We require Hiera and a valid configuration.
@@ -33,6 +31,6 @@ node default {
     import "profiles/*.pp"
 
     include base
-    hiera_include('global_profiles')
+    hiera_include('profiles')
   }
 }
